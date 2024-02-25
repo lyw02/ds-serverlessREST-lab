c7df07cc8d081efc49e4ce0b3a76f98442da4cfc
Yiwei Liu <2814824250@qq.com>
2024-02-25 12:15:35 +0000

    Exercise: Delete movie endpoint

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -118,5 +118,25 @@ export class RestAPIStack extends cdk.Stack {
       "POST",
       new apig.LambdaIntegration(newMovieFn, { proxy: true })
     );
+
+    // Delete
+    const deleteMovieFn = new lambdanode.NodejsFunction(this, "DeleteMovieFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambdas/deleteMovie.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      environment: {
+        TABLE_NAME: moviesTable.tableName,
+        REGION: "eu-west-1",
+      },
+    });
+
+    moviesTable.grantReadWriteData(deleteMovieFn);
+
+    movieEndpoint.addMethod(
+      "DELETE",
+      new apig.LambdaIntegration(deleteMovieFn, { proxy: true })
+    )
   }
 }
